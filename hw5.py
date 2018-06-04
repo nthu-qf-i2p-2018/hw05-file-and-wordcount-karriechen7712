e816a2e7fe89e0009d3e22402293ec2ba2b03615
103072120 <37106527+karriechen7712@users.noreply.github.com>
2018-06-04 19:09:29 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -8,27 +8,25 @@ import pickle
 
 
 def main(filename):
-    # read file into lines
-    lines = open(filename).readlines()
-
     # declare a word list
     all_words = []
 
     # extract all words from lines
-    for line in lines:
+    for line in open(filename):
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
         line = line.strip()
-        words = line.split()
+        if not line:
+            continue
 
         # check the format of words and append it to "all_words" list
-        for word in words:
+        for word in line.split():
             # then, remove (strip) unwanted punctuations from every word
             # "dream." => "dream"
-            wordcount = word.strip(string.punctuation)            # check if word is not empty
+            word = word.strip(string.punctuation)            # check if word is not empty
             if word:
                 # append the word to "all_words" list
-                all_words.append(wordcount)
+                all_words.append(word)
 
     # compute word count from all_words
     counter = Counter(all_words)
@@ -48,11 +46,11 @@ def main(filename):
         writer.writerows(counter.most_common())
 
     # dump to a json file named "wordcount.json"
-    json.dump(counter, open('wordcount.json', 'w'))
+    json.dump(list(counter.most_common()), open('wordcount.json', 'w'))
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
-    pickle.dump(counter, open('wordcount.json', 'wb'))
+    pickle.dump(counter, open('wordcount.pkl', 'wb'))
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
